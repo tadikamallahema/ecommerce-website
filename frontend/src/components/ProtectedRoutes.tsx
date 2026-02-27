fec36d8ef341eceb6b0c1f43a5aa87e2435a23b3
tadikamallahema <hematadikamalla@gmail.com>
2026-02-27 11:18:23 +0530

    updated gitignore

--- frontend/src/components/ProtectedRoutes.tsx
+++ frontend/src/components/ProtectedRoutes.tsx
@@ -4,18 +4,24 @@ import { Navigate } from "react-router-dom";
 
 interface protectedProps{
     children:ReactNode;
+    allowedRoles: string[];
 }
 
-const ProtectedRoutes=({children}:protectedProps)=>{
+const ProtectedRoutes=({children,allowedRoles}:protectedProps)=>{
     const [isAuthenticated,setIsAuthenticated]=useState<boolean|null>(null);
 
     useEffect(()=>{
         const checkAuth=async()=>{
             try{
-                await axios.get("http://localhost:2007/api/check",{
+                const res=await axios.get("http://localhost:2007/api/check",{
                     withCredentials:true,
                 });
-                setIsAuthenticated(true);
+                //setIsAuthenticated(true);
+            if (allowedRoles.includes(res.data.role)) {
+            setIsAuthenticated(true);
+            } else {
+          alert("You are not authorized");
+        }
             }catch(err:any){
                 const status=err?.response?.status;
                 if(status===403){
